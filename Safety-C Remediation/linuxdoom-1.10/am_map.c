fb555f3a4f777776b4fbf3b1d08a50749c74b414
Vijay R <vijayrece@citchennai.net>
2026-09-17 18:24:15 +0530

    AIForce: Update corrected am_map.c

--- Safety-C Remediation/linuxdoom-1.10/am_map.c
+++ Safety-C Remediation/linuxdoom-1.10/am_map.c
@@ -9,7 +9,7 @@
 // only under the terms of the DOOM Source Code License as
 // published by id Software. All rights reserved.
 //
-// The source is distributed in the hope that it will be useful,
+// The source is available in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
 // for more details.
@@ -419,7 +419,7 @@ void AM_findMinMaxBoundaries(void)
     fixed_t b;
 
     min_x = min_y =  MAXINT;
-    max_x = max_y = -MAXINT;
+  max_x = max_y = -MAXINT;
   
     for (i=0;i<numvertexes;i++)
     {
@@ -821,7 +821,7 @@ void AM_doFollowPlayer(void)
 //
 //
 //
-/* Removed invalid integer-to-pointer test object. */
+/* Removed invalid integer-to-pointer conversion test object. */
 
 void AM_updateLightLev(void)
 {
