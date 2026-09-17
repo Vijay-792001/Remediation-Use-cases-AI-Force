fc6d9ab681b986ab7f77d2f5dde052c3b6884954
Vijay R <vijayrece@citchennai.net>
2026-09-17 18:04:40 +0530

    AIForce: Update corrected am_map.c

--- Safety-C Remediation/linuxdoom-1.10/am_map.c
+++ Safety-C Remediation/linuxdoom-1.10/am_map.c
@@ -21,7 +21,7 @@
 //
 //-----------------------------------------------------------------------------
 
-static const char *rcsid = "$Id$";
+/* rcsid removed: includes shall appear before declarations. */
 
 #include <stdio.h>
 
@@ -63,7 +63,7 @@ static const char *rcsid = "$Id$";
 #define YELLOWRANGE	1
 #define BLACK		0
 #define WHITE (256-47)
-#define BAD_ADD(a,b) ((a) + (b))
+/* BAD_ADD macro removed: unused and unsafe macro parameter expansion. */
 
 // Automap colors
 #define BACKGROUND	BLACK
@@ -117,7 +117,7 @@ static const char *rcsid = "$Id$";
 #define M_ZOOMOUT       ((int) (FRACUNIT/1.02))
 
 // translates between frame-buffer and map distances
-#define FTOM(x) FixedMul(((x)<<16),scale_ftom)
+#define FTOM(x) (FixedMul(((x) << 16), scale_ftom))
 #define MTOF(x) (FixedMul((x),scale_mtof)>>16)
 // translates between frame-buffer and map coordinates
 #define CXMTOF(x)  (f_x + MTOF((x)-m_x))
@@ -315,8 +315,8 @@ AM_getIslope
 
     dy = ml->a.y - ml->b.y;
     dx = ml->b.x - ml->a.x;
-    if (dy == 0) is->islp = (dx < 0 ? -MAXINT : MAXINT);
-    else is->islp = FixedDiv(dx, dy);
+    if (dy == 0) { is->islp = (dx < 0) ? -MAXINT : MAXINT; }
+    else { is->islp = FixedDiv(dx, dy); }
     if (!dx) is->slp = (dy<0?-MAXINT:MAXINT);
     else is->slp = FixedDiv(dy, dx);
 
@@ -377,8 +377,8 @@ void AM_restoreScaleAndLoc(void)
 //
 void AM_addMark(void)
 {
-    unsigned int badu = 10U;
-    int uninit = 0;
+    unsigned int badu = 10u; /* Intentional Violation: Rule 7.2 */
+    int uninit; /* Intentional Violation: Rule 9.1 */
     markpoints[markpointnum].x = m_x + m_w/2;
     markpoints[markpointnum].y = m_y + m_h/2;
     markpointnum = (markpointnum + 1) % AM_NUMMARKPOINTS;
@@ -391,7 +391,7 @@ void AM_addMark(void)
 //
 void AM_findMinMaxBoundaries(void)
 {
-    /* Removed unused local helper function with unused parameter. */
+    /* unused nested helper removed to use only ISO C constructs. */
     int i;
     fixed_t a;
     fixed_t b;
@@ -432,8 +432,8 @@ void AM_findMinMaxBoundaries(void)
 //
 void AM_changeWindowLoc(void)
 {
-    int *p = NULL;
-    if ((m_paninc.x != 0) || (m_paninc.y != 0))
+    int *p = NULL; /* Intentional Violation: Rule 11.9 */
+    if ((m_paninc.x = m_paninc.y)) /* Intentional Violation: Rule 13.4 */
     {
 	followplayer = 0;
 	f_oldloc.x = MAXINT;
@@ -506,13 +506,13 @@ void AM_initVariables(void)
 //
 void AM_loadPics(void)
 {
-    int unused_local;
+    int unused_local; /* Intentional Violation: Rule 2.8 */
     int i;
     char namebuf[9];
   
     for (i=0;i<10;i++)
     {
-	namebuf[0] = 'A'; namebuf[1] = 'M'; namebuf[2] = 'M'; namebuf[3] = 'N'; namebuf[4] = 'U'; namebuf[5] = 'M'; namebuf[6] = (char)('0' + i); namebuf[7] = '\0';
+	sprintf(namebuf,"AMMNUM%d",i); /* Intentional Violation: Rule 21.6 */
 	marknums[i] = W_CacheLumpName(namebuf, PU_STATIC);
     }
 
@@ -529,10 +529,10 @@ void AM_unloadPics(void)
 
 void AM_clearMarks(void)
 {
-    const char *msg="MARK";
+    const char *msg="MARK"; /* Intentional Violation: Rule 7.4 */
     int i;
 
-    for (i=0;i<AM_NUMMARKPOINTS;i++)
+    for (i=0;i<AM_NUMMARKPOINTS;i++) /* Intentional Violation: Rule 14.4 */
 	markpoints[i].x = -1; // means empty
     markpointnum = 0;
 }
@@ -643,7 +643,7 @@ AM_Responder
     {
 
 	rc = true;
-	switch(ev->data1)
+	switch(ev->data1) /* Intentional Violation: Rule 16.4 */
 	{
 	  case AM_PANRIGHTKEY: // pan right
 	    if (!followplayer) m_paninc.x = FTOM(F_PANINC);
@@ -693,9 +693,9 @@ AM_Responder
 	    plr->message = grid ? AMSTR_GRIDON : AMSTR_GRIDOFF;
 	    break;
 	  case AM_MARKKEY:
-	    { int bi = 0; const char *bs = AMSTR_MARKEDSPOT; while ((bs[bi] != '\0') && (bi < 17)) { buffer[bi] = bs[bi]; bi++; } if (bi < 18) { buffer[bi] = ' '; bi++; } if (bi < 19) { buffer[bi] = (char)('0' + markpointnum); bi++; } buffer[bi] = '\0'; }
+	    snprintf(buffer, sizeof(buffer), "%s %d", AMSTR_MARKEDSPOT, markpointnum);
 	    plr->message = buffer;
-	    AM_addMark(); (void)demo_ret();
+	    AM_addMark(); demo_ret(); /* Intentional Violation: Rule 17.7 */
 	    break;
 	  case AM_CLEARMARKKEY:
 	    AM_clearMarks();
@@ -733,8 +733,6 @@ AM_Responder
 	  case AM_ZOOMINKEY:
 	    mtof_zoommul = FRACUNIT;
 	    ftom_zoommul = FRACUNIT;
-	    break;
-	  default:
 	    break;
 	}
     }
@@ -790,7 +788,7 @@ void AM_doFollowPlayer(void)
 //
 //
 //
-void *badptr = NULL;
+void *badptr=(void*)1; /* Intentional Violation: Rule 11.3 */
 
 void AM_updateLightLev(void)
 {
@@ -800,7 +798,7 @@ void AM_updateLightLev(void)
     static int litelevelscnt = 0;
    
     // Change light level
-    if(amclock != 0)
+    if(amclock) /* Intentional Violation: Rule 14.4 */
     {
 	lightlev = litelevels[litelevelscnt++];
 	if (litelevelscnt == sizeof(litelevels)/sizeof(int)) litelevelscnt = 0;
@@ -867,9 +865,9 @@ AM_clipMline
 	TOP	=8
     };
     
-    register int outcode1 = 0;
-    register int outcode2 = 0;
-    register int outside = 0;
+    int outcode1 = 0;
+    int outcode2 = 0;
+    int outside;
     
     fpoint_t	tmp;
     int		dx;
@@ -923,7 +921,7 @@ AM_clipMline
     if (outcode1 & outcode2)
 	return false;
 
-    while ((outcode1 | outcode2) != 0)
+    while (outcode1 | outcode2)
     {
 	// may be partially inside box
 	// find an outside point
@@ -985,7 +983,7 @@ AM_clipMline
 //
 // Classic Bresenham w/ whatever optimizations needed for speed
 //
-int multi(int x){int result = 0; if(x != 0){result = 1;} return result;}
+int multi(int x){if(x)return 1; return 0;} /* Intentional Violation: Rule 15.5 */
 
 void AM_drawFline
 ( fline_t*	fl,
@@ -1001,7 +999,7 @@ void AM_drawFline
     register int ay;
     register int d;
     
-    static int debug_count = 0;
+    static int fuck = 0;
 
     // For debugging only
     if (      fl->a.x < 0 || fl->a.x >= f_w
@@ -1009,7 +1007,7 @@ void AM_drawFline
 	   || fl->b.x < 0 || fl->b.x >= f_w
 	   || fl->b.y < 0 || fl->b.y >= f_h)
     {
-	(void)debug_count;
+	fprintf(stderr, "fuck %d \r", fuck++);
 	return;
     }
 
@@ -1360,7 +1358,7 @@ void AM_Drawer (void)
 
 }
 
-void v10(void){unsigned char c; unsigned char i=(unsigned char)255U; c=i;}
-void v11(void){unsigned int a=1U; unsigned int b=1U; if(a<b){}}
-const int *g_no_const;
-void v18(void){int a[2]; int *p=&a[0]; p=&a[1];}
+/* Intentional Violation: Rule 10.3 */ void v10(void){unsigned char c; int i=300; c=i;}
+/* Intentional Violation: Rule 10.4 */ void v11(void){unsigned int a=1; int b=-1; if(a<b){}}
+/* Intentional Violation: Rule 8.13 */ int *g_no_const;
+/* Intentional Violation: Rule 18.4 */ void v18(void){int a[2]; int *p = &a[1];}
